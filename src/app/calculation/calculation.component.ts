5f5d21f739132c96436e28dbe32a5b853a587db6
Jan Moritz Hoefner <JanMoritz.Hoefner@baloise.ch>
2023-10-18 11:35:50 +0200

    Implement insulind calculation.

--- src/app/calculation/calculation.component.ts
+++ src/app/calculation/calculation.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {StateService} from "../state.service";
-import {FoodIngredient} from "../app.component";
+import {FoodIngredient, UserProfile} from "../app.component";
+import { UserProfileService } from 'src/api/user-profile.service';
+import { Subscription, from } from 'rxjs';
 
 type WeightItem = { item: FoodIngredient, weight: number | undefined, carbSum: number };
 
@@ -9,12 +11,28 @@ type WeightItem = { item: FoodIngredient, weight: number | undefined, carbSum: n
   templateUrl: './calculation.component.html',
   styleUrls: ['./calculation.component.scss']
 })
-export class CalculationComponent implements OnInit {
+export class CalculationComponent implements OnInit, OnDestroy {
   items: FoodIngredient[] = [];
   weightPerItem: WeightItem[] = [];
   insulinSum = 0;
+  keSum = 0;
 
-  constructor(private stateService: StateService) {
+  userProfile: UserProfile = {sensitivityMorning: 0.0, sensitivityNoon: 0.0, sensitivityEvening: 0.0};
+  subscription: Subscription;
+  selectedSensitivity: number = 0
+
+
+  constructor(private stateService: StateService, private userProfileService: UserProfileService) {
+    this.subscription = from(this.userProfileService.loadProfile()).subscribe((p: UserProfile) => {
+      this.userProfile = {sensitivityMorning: p.sensitivityMorning,
+        sensitivityNoon: p.sensitivityNoon, 
+        sensitivityEvening: p.sensitivityEvening}
+      this.selectedSensitivity = p.sensitivityMorning;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   ngOnInit(): void {
@@ -29,11 +47,23 @@ export class CalculationComponent implements OnInit {
     })
   }
 
+
   private calculateInsulinSum(weightperItem: WeightItem[]): number {
     return weightperItem.reduce((accumulator, currentValue) => {
       const weight = currentValue.weight;
       return accumulator + (weight ? weight/ 100 : 0);
+    }, 0) / this.selectedSensitivity;
+  }
+
+  private calculateKeSum(weightperItem: WeightItem[]): number {
+    return weightperItem.reduce((accumulator, currentValue) => {
+      return accumulator + ((currentValue.weight ?? 0) * currentValue.item.carbohydrateUnits) ?? 0;
     }, 0);
+  }
+  
+  onSensitivityChanged(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.selectedSensitivity = parseFloat(value);
   }
 
   deleteItem(id: number) {
@@ -47,12 +77,13 @@ export class CalculationComponent implements OnInit {
     let foundItem = this.weightPerItem.find(it => it.item.id === id);
     if (foundItem) {
       foundItem.weight = parseInt((event.target as HTMLInputElement).value);
-      foundItem.carbSum = 2 * foundItem.weight; // TODO-MZ use field instead of 1
+      foundItem.carbSum = foundItem.item.carbohydrateAvailable * foundItem.weight; // TODO-MZ use field instead of 1
     }
+    this.keSum = this.calculateKeSum(this.weightPerItem);
     this.insulinSum = this.calculateInsulinSum(this.weightPerItem)
   }
 
   getWeightedValue(carbohydrateAvailable: number, weight: number|undefined) {
-    return weight ? carbohydrateAvailable / 100 * weight : 0
+    return weight ? carbohydrateAvailable * weight : 0
   }
 }
